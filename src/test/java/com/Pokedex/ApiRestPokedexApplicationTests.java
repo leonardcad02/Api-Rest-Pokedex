9a8150d64154e9260a75d63bc0a578ac725d528b
leonardcad02 <leonardcad02@gmail.com>
2021-06-14 22:54:39 -0500

    Inicialited Project

---
+++ src/test/java/com/Pokedex/ApiRestPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
